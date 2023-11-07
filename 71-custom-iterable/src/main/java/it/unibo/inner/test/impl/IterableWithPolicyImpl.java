03aa4ad8d5115ee922e9a2f8805638e2aa05c4f6
Andrea Nuvoli <andrea.nuvoli@studio.unibo.it>
2023-11-07 12:23:32 +0100

    Added javadoc comments

--- 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -10,6 +10,14 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
     private final T[] array;
     private Predicate<T> filter;
 
+    /**
+     * First costructor of class IterableWithPolicyImpl.
+     * 'array' field is passed by user.
+     * 'filter' field is setted to always return true.
+     * 
+     * @param array 
+     *
+     */
     public IterableWithPolicyImpl(final T[] array) {
         this(array, new Predicate<T>() {
 
@@ -20,24 +28,49 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
         });
     }
 
+    /**
+     * First costructor of class IterableWithPolicyImpl.
+     * Both field are passed by user.
+     * 
+     * @param array
+     * 
+     * @param filter
+     */
     public IterableWithPolicyImpl(final T[] array, final Predicate<T> filter) {
         this.array = array;
         this.filter = filter;
     }
 
+    /**
+     * Creation of a new iterator.
+     * 
+     * @return a new InnerIterator's object
+     */
     public InnerIterator iterator() {
         return new InnerIterator();
     }
 
+    /**
+     * Updating the filter with the new one passed by user.
+     */
     public void setIterationPolicy(Predicate<T> filter) {
         this.filter = filter;
     }
 
 
+    /**
+     * This inner class implements an iterator in order to
+     * filter the elements during the iteration according to
+     * the policy in 'filter' field.
+     */
     class InnerIterator implements Iterator<T> {
 
         private int currentIndex;
 
+        /**
+         * @return true 
+         *          if the iteration has more elements.
+         */
         public boolean hasNext() {
             while(currentIndex < array.length) {
                 T elementOfArray = array[currentIndex];
@@ -49,6 +82,9 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
             return false;
         }
 
+        /**
+         * @return the next element in the iteration.
+         */
         public T next() {
             if(hasNext()) {
                 T elementOfArray = array[currentIndex];
